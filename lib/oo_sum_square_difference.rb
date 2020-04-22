e2fc587834fba44d5937c3081f744348bb3ff256
github username <github email address>
2020-04-22 09:36:30 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,20 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+    attr_accessor :number, :difference
+  
+    def initialize(number)
+      @number = number
+    end
+  
+    def sum_squares(number)
+        (1..number).map{|x| x** 2}.sum
+    end
+  
+    def square_sum(number)
+        (1..number).sum ** 2
+    end
+  
+    def difference
+      square_sum(number) - sum_squares(number)
+    end
+  end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+def sum_square_difference(number)
+    square_sum(number) - sum_squares(number)
+  end
+  
+  def sum_squares(number)
+    
+    (1..number).map{|x| x** 2}.sum
+    
+  end
+  
+  def square_sum(number)
+    
+    (1..number).sum ** 2
+    
+  end
